87510719995d55f6294913c4699babf5d176eafa
kubal-s <kubal.s@husky.neu.edu>
2020-07-20 16:46:46 -0400

    added success failure messages to to UI for forgot password fucntionality

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -11,6 +11,7 @@ export class ForgotPasswordComponent implements OnInit {
   forgotPasswordForm: FormGroup;
   error = false;
   errorList;
+  message ='';
   email = new FormControl('', [Validators.required, Validators.email]);
   constructor(private authService: AuthService) { }
 
@@ -30,7 +31,17 @@ export class ForgotPasswordComponent implements OnInit {
   onSubmit() {
     this.authService.forgotPassword(this.forgotPasswordForm.value).subscribe(
       data => {
-       
+        console.log(data)
+        if(data.hasOwnProperty('MessageId')){
+          this.error= false;
+          this.message = "Request submitted successfully with request id : "+
+           data.ResponseMetadata.RequestId+"\n"+", where message id : "+ data.MessageId;
+        }
+        else if(data.hasOwnProperty('message')){
+          this.errorList = data
+          this.error = true;
+        }
+          
       },
       err => {
 
