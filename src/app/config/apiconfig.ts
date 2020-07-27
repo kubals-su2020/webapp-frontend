0f658e44157211a3e873c9364cb81e314d766879
kubal-s <kubal.s@husky.neu.edu>
2020-07-27 12:44:29 -0400

    http to https connection to node server

--- src/app/config/apiconfig.ts
+++ src/app/config/apiconfig.ts
@@ -1,4 +1,5 @@
 export const apiconfig = {
-    base_url :"http://"+window.location.hostname+":3000"
+    // base_url :"http://"+window.location.hostname+":3000"
+    base_url :"https://"+window.location.hostname+":3000"
 };
  
